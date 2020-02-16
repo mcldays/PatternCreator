74714cb425b43776bb61251e3c6a539d73498452
krendel338 <lexlex97@mail.ru>
2020-02-16 16:32:16 +0400

    начал делать обработку шаблонов

---
+++ PatternCreator/Controllers/PrintController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace PatternCreator.Controllers
+{
+    public class PrintController : Controller
+    {
+        [HttpPost]
+        public string[] GetComputedPhotos(string data)
+        {
+            var photos = new List<string>();
+            var users = Utilities.SendDbUtility.GetAllUsers();
+            var templates = Utilities.SendDbUtility.GetAllTemplates();
+            var positions = Utilities.SendDbUtility.GetAllPositions();
+
+            var parsed = JsonConvert.DeserializeObject<Dictionary<string, List<int>>>(data);
+            foreach (var user in parsed["users"])
+            {
+                foreach (var template in parsed["templates"])
+                {
+                    photos.Add(Utilities.ComputePhoto.Compute(users.Find(t=>t.Id == user), templates.Find(t=>t.Id == template), positions.Where(t => t.PictureId == template).ToList()));
+                }
+            }
+
+            return photos.ToArray();
+        }
+    }
+}
---
+++ PatternCreator/Utilities/ComputePhoto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Web;
+using PatternCreator.Models;
+
+namespace PatternCreator.Utilities
+{
+    public static class ComputePhoto
+    {
+        public static string Compute(UserModel user, PictureModel template, List<PositionModel> positions)
+        {
+            Bitmap bmp;
+
+            RectangleF rectf = new RectangleF(70, 90, 90, 50);
+
+            using (var ms = new MemoryStream(template.Image))
+                bmp = new Bitmap(ms);
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            foreach (var position in positions)
+            {
+                g.DrawString("yourText", new Font("Tahoma", 8), Brushes.Black, rectf);
+            }
+
+            g.Flush();
+            return "";
+        }
+    }
+}
